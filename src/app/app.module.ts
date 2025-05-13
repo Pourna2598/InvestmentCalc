e9fe12829fdb77b22bd667963fcfb22a55d2a6fe
Pourna2598 <107390495+Pourna2598@users.noreply.github.com>
2025-05-13 20:02:18 +0530

    multiple modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { UserInputComponent } from "./user-input/user-input.component";
 import { ExpenseResultsComponent } from "./expense-results/expense-results.component";
 import { FormsModule } from "@angular/forms";
+import { UserInputModule } from "./user-input/user-input.module";
 
 @NgModule({
     declarations:[
         AppComponent,
         HeaderComponent,
-        UserInputComponent,
         ExpenseResultsComponent
     ],
-    imports:[FormsModule],
+    imports:[UserInputModule],
     bootstrap: [AppComponent]
 })
 export class AppModule{}
